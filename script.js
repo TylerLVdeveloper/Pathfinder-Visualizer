4368f8b6798d3e4af7fc62dbdc7ad460f837a0c8
tylerLVdeveloper <tylerd062292@gmail.com>
2021-08-03 16:42:53 -0700

    second starting commit

--- script.js
+++ script.js
@@ -105,8 +105,108 @@ const boardSpaces = [
 ];
 
 boardSpaces.forEach(function (row, rowIndex) {
-  row.forEach(function (space) {
-    const spaceHTML = `<div class="space" id="${rowIndex + 1}-${space}"></div>`;
-    board.insertAdjacentHTML("afterbegin", spaceHTML);
-  });
+  for(let i = row.length; i--; i>=0){
+    const spaceHTML = `<div class="space" id="${rowIndex + 1}-${row[i]}"></div>`;
+board.insertAdjacentHTML("afterbegin", spaceHTML);
+  }
 });
+
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+class NodeCl {
+  constructor(coord, gCost, hCost, prevNode){
+    this.gCost = gCost;
+    this.hCost = hCost;
+    this.fCost = this.gCost + this.hCost;
+
+    this.coord = coord;
+    this.prevNode = prevNode;
+  }
+
+  recalcCosts(gCost, hCost){
+    this.gCost = gCost;
+    this.hCost = hCost;
+    this.fCost = this.gCost + this.hCost;
+}
+}
+
+const endNodeClass = 'end_node_class';
+const startNodeClass = 'start_node_class';
+const openNodeClass = 'open_node_class';
+
+let endNode = "20-12";
+const startNode = new NodeCl("3-8", 0, 0, 'none');
+
+
+document.getElementById(endNode).classList.add(endNodeClass);
+document.getElementById(startNode.coord).classList.add(startNodeClass);
+
+const openNodes = [];
+const closedNodes = [];
+
+const calcHCost = function(node){
+   const splitCoordsEndNode = endNode.slice().split('-');
+   const yCoordEndNode = +splitCoordsEndNode[0];
+   const xCoordEndNode = +splitCoordsEndNode[1];
+
+   const splitCoordsCurrNode = node.slice().split('-');
+   const yCoordCurrNode = +splitCoordsCurrNode[0];
+   const xCoordCurrNode = +splitCoordsCurrNode[1];
+   
+   const hCost = ((yCoordEndNode - yCoordCurrNode) * 10) + ((xCoordEndNode - xCoordCurrNode) * 10);
+   return hCost;
+   
+}
+
+
+
+const calcNeighborNodes = function(currentNode){
+   const splitCoords = currentNode.coord.slice().split('-');
+   const yCoord = splitCoords[0];
+   const xCoord = splitCoords[1];
+   
+   /////////////////////////////////////////////////////////////////// NORTH NEIGHBOR NODE
+   // calculate north neighbor coordinates
+   const northCoord = ((+yCoord + 1).toString() + '-' + xCoord);
+
+   // calculate north neighbor H cost
+   const northHCost = calcHCost(northCoord);
+
+   // create north neighbor object 
+   const northNeighbor = new NodeCl(northCoord, (currentNode.gCost + 10), northHCost, currentNode.coord);
+   console.log(northNeighbor);
+
+
+   /////////////////////////////////////////////////////////////////// NORTH-EAST NEIGHBOR NODE
+   const northEastCoord = (+yCoord + 1).toString() + '-' + (+xCoord + 1).toString();
+
+   const northEastHCost = calcHCost(northEastCoord);
+
+   const northEastNeighbor = new NodeCl(northEastCoord, (currentNode.gCost + 14), northEastHCost, currentNode.coord);
+   console.log(northEastNeighbor);
+
+
+
+   const eastNeighbor = yCoord + '-' + (+xCoord + 1).toString();
+   const southEastNeighbor = (+yCoord - 1).toString() + '-' + (+xCoord + 1).toString();
+   const southNeighbor = (+yCoord - 1).toString() + '-' + +xCoord;
+   const southWestNeighbor = (+yCoord - 1).toString() + '-' + (+xCoord - 1).toString();
+   const westNeighbor = yCoord + '-' + (+xCoord - 1).toString();
+   const northWestNeighbor = (+yCoord + 1).toString() + '-' + (+xCoord - 1).toString();
+   
+   document.getElementById(`${northNeighbor.coord}`).classList.add(openNodeClass);
+   document.getElementById(`${northNeighbor.coord}`).textContent = 'N';
+
+   document.getElementById(`${northEastNeighbor.coord}`).classList.add(openNodeClass);
+   document.getElementById(`${northEastNeighbor.coord}`).textContent = 'NE';
+   
+   document.getElementById(eastNeighbor).textContent = 'E';
+   document.getElementById(southEastNeighbor).textContent = 'SE';
+   document.getElementById(southNeighbor).textContent = 'S';
+   document.getElementById(southWestNeighbor).textContent = 'SW';
+   document.getElementById(westNeighbor).textContent = 'W';
+   document.getElementById(northWestNeighbor).textContent = 'NW';
+}
+
+calcNeighborNodes(startNode);
